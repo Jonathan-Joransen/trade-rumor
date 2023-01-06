de4ecfc10783918d8d04700e51a26d8477170c2a
jonathan <jonathan.joransen@gmail.com>
2023-01-06 00:26:46 -0500

    changed basepath

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,7 +3,7 @@ import { SessionProvider, useSession } from 'next-auth/react'
 
 export default function App({ Component, pageProps }) {
   return ( 
-  <SessionProvider session={pageProps.session}> 
+  <SessionProvider session={pageProps.session} basePath="https://nbatraderumor.com"> 
       {Component.auth === true ? (
         <Auth>
           <Component {...pageProps} />
