0dda328982b2981cba15529b09bc585602531ff3
jonathan <jonathan.joransen@gmail.com>
2023-01-16 12:53:28 -0500

    update free agent popup to have one option : :

--- components/PlayerSelectionList.tsx
+++ components/PlayerSelectionList.tsx
@@ -128,17 +128,12 @@ export const PlayerSelectionList = ({
       (player as RosterPlayer).isUpcomingFreeAgent === true)
     {
       let rosterPlayer = player as RosterPlayer;
-      let teamOptionUserInput = await getPopUpUserInput(
+      let freeAgentUserInput = await getPopUpUserInput(
         new PopUpOptions("Upcoming Free Agent",
           `${rosterPlayer.firstName} ${rosterPlayer.lastName} is an upcoming free agent and cannot be traded.`,
-          ["Cancel", "OK"],
+          ["OK"],
           updatePopUpUserInput));
-          if (teamOptionUserInput === "Cancel") {
-            return el.classList.remove(styles.selectedPlayer);
-          }
-          if (teamOptionUserInput === "OK" 
-          && teamNames.length === 2 
-          && rosterPlayer.teamsCannotBeTradedTo.includes(teamNames.filter((t) => t !== player.teamName)[0])) {
+          if (freeAgentUserInput === "OK") {
             return el.classList.remove(styles.selectedPlayer);
           }
     }
